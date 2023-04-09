739945a4b6afdd9d255cdab12715848f15069e17
Marek Novosad <manoxik@seznam.cz>
2023-04-09 16:01:58 +0200

    DrawCall added

---
+++ ShevaEngine.Core/Graphics/DrawCall.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ShevaEngine.Core.Graphics;
+
+internal struct DrawCall
+{
+    public readonly int MaterialIndex;
+    public readonly int MeshPartIndex;
+    public readonly Matrix Transform;
+
+
+    public DrawCall(int materialIndex, int meshPartIndex, in Matrix transform)
+    {
+        MaterialIndex = materialIndex;
+        MeshPartIndex = meshPartIndex;
+        Transform = transform;
+    }
+}
